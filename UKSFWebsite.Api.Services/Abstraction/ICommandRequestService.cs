360dfa2306ad387246ea59bc42a05b4bfd554515
Tim Beswick <contact.tim.here@gmail.com>
2019-11-06 22:26:22 +0000

    Revert review state if request errors

--- UKSFWebsite.Api.Services/Abstraction/ICommandRequestService.cs
+++ UKSFWebsite.Api.Services/Abstraction/ICommandRequestService.cs
@@ -6,7 +6,7 @@ namespace UKSFWebsite.Api.Services.Abstraction {
         Task Add(CommandRequest request, ChainOfCommandMode mode = ChainOfCommandMode.COMMANDER_AND_ONE_ABOVE);
         Task ArchiveRequest(string id);
         Task SetRequestReviewState(CommandRequest request, string reviewerId, ReviewState newState);
-        Task SetRequestAllReviewStates(CommandRequest request, ReviewState newState, string overriderId);
+        Task SetRequestAllReviewStates(CommandRequest request, ReviewState newState);
         ReviewState GetReviewState(string id, string reviewer);
         bool IsRequestApproved(string id);
         bool IsRequestRejected(string id);
