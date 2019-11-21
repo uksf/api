e8c8d0559ee9a7a9e7dfa321115275e62aa15c5e
Tim Beswick <contact.tim.here@gmail.com>
2019-11-21 22:32:29 +0000

    Properly close process window before killing, more graceful for teamspeak when signalr not connected. More delay between teamspeak up checks

--- UKSFWebsite.Api.Services/Utility/ProcessHelper.cs
+++ UKSFWebsite.Api.Services/Utility/ProcessHelper.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Management;
+using System.Runtime.InteropServices;
 using Microsoft.Win32.TaskScheduler;
 using Task = System.Threading.Tasks.Task;
 
 namespace UKSFWebsite.Api.Services.Utility {
     public static class ProcessHelper {
+        private const int SC_CLOSE = 0xF060;
+        private const int WM_SYSCOMMAND = 0x0112;
+
         public static int LaunchManagedProcess(string executable, string arguments = null) {
             int processId = default;
             using ManagementClass managementClass = new ManagementClass("Win32_Process");
@@ -23,12 +27,19 @@ namespace UKSFWebsite.Api.Services.Utility {
             return processId;
         }
 
-        public static async Task LaunchProcess(string name, string command) {
+        public static async Task LaunchExternalProcess(string name, string command) {
             using TaskDefinition taskDefinition = TaskService.Instance.NewTask();
             taskDefinition.Actions.Add(new ExecAction("cmd", $"/C {command}"));
             taskDefinition.Triggers.Add(new TimeTrigger(DateTime.Now.AddSeconds(1)));
             TaskService.Instance.RootFolder.RegisterTaskDefinition(name, taskDefinition);
             await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int PostMessage(IntPtr hwnd, int msg, int wparam, int lparam);
+
+        public static void CloseProcessGracefully(IntPtr mainWindowHandle) {
+            PostMessage(mainWindowHandle, WM_SYSCOMMAND, SC_CLOSE, 0);
         }
     }
 }
