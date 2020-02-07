a8604f20d1a7285c5589012f684d0cefecab6ad2
Tim Beswick <contact.tim.here@gmail.com>
2020-02-07 17:54:56 +0000

    Added variables service tests

--- UKSF.Api.Services/Common/DisplayNameUtilities.cs
+++ UKSF.Api.Services/Common/DisplayNameUtilities.cs
@@ -11,22 +11,22 @@ namespace UKSF.Api.Services.Common {
     public static class DisplayNameUtilities {
         public static string ConvertObjectIds(this string message) {
             string newMessage = message;
-            if (!string.IsNullOrEmpty(message)) {
-                IDisplayNameService displayNameService = ServiceWrapper.ServiceProvider.GetService<IDisplayNameService>();
-                IUnitsService unitsService = ServiceWrapper.ServiceProvider.GetService<IUnitsService>();
-                IEnumerable<string> parts = Regex.Split(message, @"\s+").Where(s => s != string.Empty);
-                foreach (string part in parts) {
-                    if (ObjectId.TryParse(part, out ObjectId _)) {
-                        string displayName = displayNameService.GetDisplayName(part);
-                        if (displayName == part) {
-                            Unit unit = unitsService.Data().GetSingle(x => x.id == part);
-                            if (unit != null) {
-                                displayName = unit.name;
-                            }
-                        }
+            if (string.IsNullOrEmpty(message)) return newMessage;
 
-                        newMessage = newMessage.Replace(part, displayName);
+            IDisplayNameService displayNameService = ServiceWrapper.ServiceProvider.GetService<IDisplayNameService>();
+            IUnitsService unitsService = ServiceWrapper.ServiceProvider.GetService<IUnitsService>();
+            IEnumerable<string> parts = Regex.Split(message, @"\s+").Where(s => s != string.Empty);
+            foreach (string part in parts) {
+                if (ObjectId.TryParse(part, out ObjectId _)) {
+                    string displayName = displayNameService.GetDisplayName(part);
+                    if (displayName == part) {
+                        Unit unit = unitsService.Data().GetSingle(x => x.id == part);
+                        if (unit != null) {
+                            displayName = unit.name;
+                        }
                     }
+
+                    newMessage = newMessage.Replace(part, displayName);
                 }
             }
 
---
+++ UKSF.Tests.Unit/Services/Admin/VariablesServiceTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using UKSF.Api.Models.Admin;
+using UKSF.Api.Services.Admin;
+using UKSF.Common;
+using Xunit;
+
+namespace UKSF.Tests.Unit.Services.Admin {
+    public class VariablesServiceTests {
+        [Fact]
+        public void ShouldGetVariableAsString() {
+            const string EXPECTED = "Value";
+            VariableItem variableItem = new VariableItem {key = "Test", item = EXPECTED};
+
+            string subject = variableItem.AsString();
+
+            subject.Should().Be(EXPECTED);
+        }
+
+        [Fact]
+        public void ShouldGetVariableAsDouble() {
+            const double EXPECTED = 1.5;
+            VariableItem variableItem = new VariableItem {key = "Test", item = EXPECTED};
+
+            double subject = variableItem.AsDouble();
+
+            subject.Should().Be(EXPECTED);
+        }
+
+        [Fact]
+        public void ShouldGetVariableAsBool() {
+            const bool EXPECTED = true;
+            VariableItem variableItem = new VariableItem {key = "Test", item = EXPECTED};
+
+            bool subject = variableItem.AsBool();
+
+            subject.Should().Be(EXPECTED);
+        }
+
+        [Fact]
+        public void ShouldGetVariableAsUlong() {
+            const ulong EXPECTED = ulong.MaxValue;
+            VariableItem variableItem = new VariableItem {key = "Test", item = EXPECTED};
+
+            ulong subject = variableItem.AsUlong();
+
+            subject.Should().Be(EXPECTED);
+        }
+
+        [Fact]
+        public void ShouldGetVariableAsArray() {
+            VariableItem variableItem = new VariableItem {key = "Test", item = "item1,item2, item3"};
+
+            string[] subject = variableItem.AsArray();
+
+            subject.Should().HaveCount(3);
+            subject.Should().Contain(new string[] {"item1", "item2", "item3"});
+        }
+
+        [Fact]
+        public void ShouldGetVariableAsArrayWithPredicate() {
+            VariableItem variableItem = new VariableItem {key = "Test", item = "\"item1\",item2"};
+
+            string[] subject = variableItem.AsArray(x => x.RemoveQuotes());
+
+            subject.Should().HaveCount(2);
+            subject.Should().Contain(new string[] {"item1", "item2"});
+        }
+
+        [Fact]
+        public void ShouldGetVariableAsDoublesArray() {
+            VariableItem variableItem = new VariableItem {key = "Test", item = "1.5,1.67845567657, -0.000000456"};
+
+            List<double> subject = variableItem.AsDoublesArray().ToList();
+
+            subject.Should().HaveCount(3);
+            subject.Should().Contain(new double[] {1.5, 1.67845567657, -0.000000456});
+        }
+
+        [Fact]
+        public void ShouldHaveItem() {
+            VariableItem variableItem = new VariableItem {key = "Test", item = "test"};
+
+            Action act = () => variableItem.AssertHasItem();
+
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void ShouldThrowWithNoItem() {
+            VariableItem variableItem = new VariableItem {key = "Test"};
+
+            Action act = () => variableItem.AssertHasItem();
+
+            act.Should().Throw<Exception>();
+        }
+
+        [Fact]
+        public void ShouldThrowWithInvalidDouble() {
+            VariableItem variableItem = new VariableItem {key = "Test", item = "wontwork"};
+
+            Action act = () => variableItem.AsDouble();
+
+            act.Should().Throw<InvalidCastException>();
+        }
+
+        [Fact]
+        public void ShouldThrowWithInvalidBool() {
+            VariableItem variableItem = new VariableItem {key = "Test", item = "wontwork"};
+
+            Action act = () => variableItem.AsBool();
+
+            act.Should().Throw<InvalidCastException>();
+        }
+
+        [Fact]
+        public void ShouldThrowWithInvalidUlong() {
+            VariableItem variableItem = new VariableItem {key = "Test", item = "wontwork"};
+
+            Action act = () => variableItem.AsUlong();
+
+            act.Should().Throw<InvalidCastException>();
+        }
+    }
+}
