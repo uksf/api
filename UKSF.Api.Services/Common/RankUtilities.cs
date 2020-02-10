f38b1773839dcb6c59b8caffe9b2b0e03a7fb999
Tim Beswick <contact.tim.here@gmail.com>
2020-02-10 13:21:45 +0000

    Added null tests for rank utilities sort

--- UKSF.Api.Services/Common/RankUtilities.cs
+++ UKSF.Api.Services/Common/RankUtilities.cs
@@ -2,9 +2,9 @@
 
 namespace UKSF.Api.Services.Common {
     public static class RankUtilities {
-        public static int Sort(this Rank rankA, Rank rankB) {
-            int rankOrderA = rankA?.order ?? 0;
-            int rankOrderB = rankB?.order ?? 0;
+        public static int Sort(Rank rankA, Rank rankB) {
+            int rankOrderA = rankA?.order ?? int.MaxValue;
+            int rankOrderB = rankB?.order ?? int.MaxValue;
             return rankOrderA < rankOrderB ? -1 : rankOrderA > rankOrderB ? 1 : 0;
         }
     }
