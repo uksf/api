20273621ee225374fe4d0c9414c1eda597319557
Tim Beswick <contact.tim.here@gmail.com>
2020-05-25 13:43:28 +0100

    Remove Integrations
    - Merged Integrations functionality into main API
    - Added some error checking in communications controller

--- UKSF.Api.Interfaces/Utility/ISchedulerService.cs
+++ UKSF.Api.Interfaces/Utility/ISchedulerService.cs
@@ -6,7 +6,6 @@ using UKSF.Api.Models.Utility;
 namespace UKSF.Api.Interfaces.Utility {
     public interface ISchedulerService : IDataBackedService<ISchedulerDataService> {
         void LoadApi();
-        void LoadIntegrations();
         Task Create(DateTime next, TimeSpan interval, string action, params object[] actionParameters);
         Task Cancel(Func<ScheduledJob, bool> predicate);
     }
--- UKSF.Integrations/Global.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace UKSF.Integrations {
-    public static class Global {
-        public static IServiceProvider ServiceProvider;
-    }
-}
