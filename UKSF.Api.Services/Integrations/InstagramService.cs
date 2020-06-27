44cc23b784f8fbf86d7b19b0937dded8f7e484ab
Tim Beswick <contact.tim.here@gmail.com>
2020-06-27 14:30:22 +0100

    Wrap instagram in try

--- UKSF.Api.Services/Integrations/InstagramService.cs
+++ UKSF.Api.Services/Integrations/InstagramService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -12,35 +13,41 @@ namespace UKSF.Api.Services.Integrations {
         private List<InstagramImage> images = new List<InstagramImage>();
 
         public async Task CacheInstagramImages() {
-            LogWrapper.Log("Running instagram get");
-            using HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("https://www.instagram.com/uksfmilsim/?__a=1");
-            if (!response.IsSuccessStatusCode) {
-                LogWrapper.Log($"Failed to get instagram images, error: {response}");
-                return;
+            try {
+                LogWrapper.Log("Running instagram get");
+                using HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://www.instagram.com/uksfmilsim/?__a=1");
+                if (!response.IsSuccessStatusCode) {
+                    LogWrapper.Log($"Failed to get instagram images, error: {response}");
+                    return;
+                }
+
+                string contentString = await response.Content.ReadAsStringAsync();
+                JObject contentObject = JObject.Parse(contentString);
+                JToken imagesToken = contentObject["graphql"]?["user"]?["edge_owner_to_timeline_media"]?["edges"];
+
+                if (imagesToken == null) {
+                    LogWrapper.Log($"Instagram response contains no images: {contentObject}");
+                    return;
+                }
+
+                List<InstagramImage> allNewImages = imagesToken.Select(x => new InstagramImage { shortcode = x["node"]?["shortcode"]?.ToString(), url = x["node"]?["display_url"]?.ToString() })
+                                                               .ToList();
+                if (images.Count > 0 && allNewImages.First().shortcode == images.First().shortcode) {
+                    // Most recent image is the same, therefore all images are already present
+                    LogWrapper.Log("No instagram images processed");
+                    return;
+                }
+
+                // Isolate new shortcodes, insert at start of list, and take only 12
+                IEnumerable<InstagramImage> newImages = allNewImages.Where(x => images.All(y => x.shortcode != y.shortcode));
+                images.InsertRange(0, newImages);
+                images = images.Take(12).ToList();
+                LogWrapper.Log($"Insta images: {images}");
+
+            } catch (Exception exception) {
+                LogWrapper.Log(exception);
             }
-
-            string contentString = await response.Content.ReadAsStringAsync();
-            JObject contentObject = JObject.Parse(contentString);
-            JToken imagesToken = contentObject["graphql"]?["user"]?["edge_owner_to_timeline_media"]?["edges"];
-
-            if (imagesToken == null) {
-                LogWrapper.Log($"Instagram response contains no images: {contentObject}");
-                return;
-            }
-
-            List<InstagramImage> allNewImages = imagesToken.Select(x => new InstagramImage { shortcode = x["node"]?["shortcode"]?.ToString(), url = x["node"]?["display_url"]?.ToString() }).ToList();
-            if (images.Count > 0 && allNewImages.First().shortcode == images.First().shortcode) {
-                // Most recent image is the same, therefore all images are already present
-                LogWrapper.Log("No instagram images processed");
-                return;
-            }
-
-            // Isolate new shortcodes, insert at start of list, and take only 12
-            IEnumerable<InstagramImage> newImages = allNewImages.Where(x => images.All(y => x.shortcode != y.shortcode));
-            images.InsertRange(0, newImages);
-            images = images.Take(12).ToList();
-            LogWrapper.Log($"Insta images: {images}");
         }
 
         public List<InstagramImage> GetImages() => images;
