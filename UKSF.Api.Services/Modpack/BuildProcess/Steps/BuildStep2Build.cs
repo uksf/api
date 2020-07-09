1f02d8b765b7f51e612dc1bf03090742b34c035a
Tim Beswick <contact.tim.here@gmail.com>
2020-07-09 16:43:08 +0100

    Added new build functionality. Moved steps to own service

---
+++ UKSF.Api.Services/Modpack/BuildProcess/Steps/BuildStep2Build.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using UKSF.Common;
+
+namespace UKSF.Api.Services.Modpack.BuildProcess.Steps {
+    public class BuildStep2Build : BuildStep {
+        public const string NAME = "Build";
+
+        public override async Task Setup() {
+            await base.Setup();
+            await TaskUtilities.Delay(TimeSpan.FromSeconds(1), CancellationTokenSource.Token);
+            await BuildProcessHelper.RunPowershell(Logger, CancellationTokenSource.Token, "echo", "C:/", "y45 4y5 dgfh");
+        }
+
+        public override async Task Process() {
+            await base.Process();
+            await TaskUtilities.Delay(TimeSpan.FromSeconds(1), CancellationTokenSource.Token);
+            await BuildProcessHelper.RunPowershell(Logger, CancellationTokenSource.Token, "echo", "C:/", "dfghfgh");
+            await BuildProcessHelper.RunPowershell(Logger, CancellationTokenSource.Token, "echo", "C:/", "h fgd");
+            await BuildProcessHelper.RunPowershell(Logger, CancellationTokenSource.Token, "echo", "C:/", "456");
+        }
+
+        public override async Task Teardown() {
+            await base.Teardown();
+            await TaskUtilities.Delay(TimeSpan.FromSeconds(1), CancellationTokenSource.Token);
+            await BuildProcessHelper.RunPowershell(Logger, CancellationTokenSource.Token, "echo", "C:/", "hfg");
+        }
+    }
+}
