cff795616ef0c912036830e952f8a2c5470aa064
Tim Beswick <contact.tim.here@gmail.com>
2020-10-24 11:49:42 +0100

    Resolve UKSF into JSFAW

--- UKSF.Api.Models/Mission/MissionUnit.cs
+++ UKSF.Api.Models/Mission/MissionUnit.cs
@@ -4,7 +4,6 @@ using UKSF.Api.Models.Units;
 namespace UKSF.Api.Models.Mission {
     public class MissionUnit {
         public string callsign;
-        public int depth;
         public List<MissionPlayer> members = new List<MissionPlayer>();
         public Dictionary<string, MissionPlayer> roles = new Dictionary<string, MissionPlayer>();
         public Unit sourceUnit;
