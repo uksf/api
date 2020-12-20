03d2e9a07f0dadd1b8ff74599272b7cc322c1748
Tim Beswick <contact.tim.here@gmail.com>
2020-12-20 16:34:13 +0000

    Basic projects

---
+++ Tests/UKSF.Api.Documents.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using UKSF.Api.Documents.Controllers;
+using UKSF.Api.Tests.Common;
+using Xunit;
+
+namespace UKSF.Api.Documents.Tests {
+    public class DependencyInjectionTests : DependencyInjectionTestsBase {
+        public DependencyInjectionTests() {
+
+        }
+
+        [Fact]
+        public void When_resolving_controllers() {
+            Services.AddTransient<DocumentsController>();
+            ServiceProvider serviceProvider = Services.BuildServiceProvider();
+
+            serviceProvider.GetRequiredService<DocumentsController>().Should().NotBeNull();
+        }
+
+        [Fact]
+        public void When_resolving_event_handlers() {
+            // Services.AddTransient<CommandRequestEventHandler>();
+            // ServiceProvider serviceProvider = Services.BuildServiceProvider();
+            //
+            // serviceProvider.GetRequiredService<CommandRequestEventHandler>().Should().NotBeNull();
+        }
+    }
+}
---
+++ UKSF.Api.Documents/ApiDocumentsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UKSF.Api.Documents {
+    public static class ApiDocumentsExtensions {
+        public static IServiceCollection AddUksfDocuments(this IServiceCollection services) => services.AddContexts().AddEventHandlers().AddServices();
+
+        private static IServiceCollection AddContexts(this IServiceCollection services) => services;
+
+        private static IServiceCollection AddEventHandlers(this IServiceCollection services) => services;
+
+        private static IServiceCollection AddServices(this IServiceCollection services) => services;
+
+        public static void AddUksfDocumentsSignalr(this IEndpointRouteBuilder builder) { }
+    }
+}
