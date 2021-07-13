aa89a7cf37aadd73ae16d13e17d47e038ea26f35
Tim Beswick <contact.tim.here@gmail.com>
2021-07-13 21:15:20 +0100

    Move optionals for acre

--- UKSF.Api.Modpack/Services/BuildProcess/Steps/BuildSteps/Mods/BuildStepBuildAcre.cs
+++ UKSF.Api.Modpack/Services/BuildProcess/Steps/BuildSteps/Mods/BuildStepBuildAcre.cs
@@ -10,6 +10,7 @@ namespace UKSF.Api.Modpack.Services.BuildProcess.Steps.BuildSteps.Mods
     {
         public const string NAME = "Build ACRE";
         private const string MOD_NAME = "acre";
+        private readonly List<string> _allowedOptionals = new() { "acre_sys_gm", "acre_sys_sog" };
 
         private readonly List<string> _errorExclusions = new() { "Found DirectX", "Linking statically", "Visual Studio 16", "INFO: Building", "Build Type" };
 
@@ -26,13 +27,30 @@ namespace UKSF.Api.Modpack.Services.BuildProcess.Steps.BuildSteps.Mods
                 StepLogger.LogSurround("\nRunning make.py...");
                 BuildProcessHelper processHelper =
                     new(StepLogger, CancellationTokenSource, errorExclusions: _errorExclusions, ignoreErrorGateClose: "File written to", ignoreErrorGateOpen: "MakePbo Version");
-                processHelper.Run(toolsPath, PythonPath, MakeCommand("redirect compile"), (int) TimeSpan.FromMinutes(10).TotalMilliseconds);
+                processHelper.Run(toolsPath, PythonPath, MakeCommand("redirect compile"), (int)TimeSpan.FromMinutes(10).TotalMilliseconds);
                 StepLogger.LogSurround("Make.py complete");
             }
 
             StepLogger.LogSurround("\nMoving ACRE release to build...");
             await CopyDirectory(releasePath, buildPath);
             StepLogger.LogSurround("Moved ACRE release to build");
+
+            StepLogger.LogSurround("\nMoving optionals...");
+            await MoveOptionals(buildPath);
+            StepLogger.LogSurround("Moved optionals");
+        }
+
+        private async Task MoveOptionals(string buildPath)
+        {
+            string optionalsPath = Path.Join(buildPath, "optionals");
+            string addonsPath = Path.Join(buildPath, "addons");
+            DirectoryInfo addons = new(addonsPath);
+            foreach (string optionalName in _allowedOptionals)
+            {
+                DirectoryInfo optional = new(Path.Join(optionalsPath, $"@{optionalName}", "addons"));
+                List<FileInfo> files = GetDirectoryContents(optional);
+                await CopyFiles(optional, addons, files);
+            }
         }
     }
 }
