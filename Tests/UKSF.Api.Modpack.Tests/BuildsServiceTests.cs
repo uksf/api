a9d910840f229288532d959a1425784f57b2be2b
Tim Beswick <contact.tim.here@gmail.com>
2021-07-19 20:55:41 +0100

    Remove now invalid test

--- Tests/UKSF.Api.Modpack.Tests/BuildsServiceTests.cs
+++ Tests/UKSF.Api.Modpack.Tests/BuildsServiceTests.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Moq;
-using UKSF.Api.ArmaServer.Models;
+﻿using Moq;
 using UKSF.Api.Modpack.Context;
-using UKSF.Api.Modpack.Models;
 using UKSF.Api.Modpack.Services;
 using UKSF.Api.Modpack.Services.BuildProcess;
 using UKSF.Api.Personnel.Context;
-using UKSF.Api.Personnel.Models;
 using UKSF.Api.Shared.Events;
 using UKSF.Api.Shared.Services;
-using Xunit;
 
 namespace UKSF.Api.Modpack.Tests
 {
@@ -34,20 +26,20 @@ namespace UKSF.Api.Modpack.Tests
             _subject = new(_mockBuildsContext.Object, _mockBuildStepService.Object, _mockAccountContext.Object, mockHttpContextService.Object, mockLogger.Object);
         }
 
-        [Fact]
-        public async Task When_creating_rc_build()
-        {
-            _mockBuildsContext.Setup(x => x.Get(It.IsAny<Func<ModpackBuild, bool>>())).Returns(new List<ModpackBuild>());
-            _mockAccountContext.Setup(x => x.GetSingle(It.IsAny<Func<DomainAccount, bool>>())).Returns(new DomainAccount { Id = "accountId" });
-            _mockBuildStepService.Setup(x => x.GetSteps(GameEnvironment.RC)).Returns(new List<ModpackBuildStep>());
-
-            GithubCommit githubCommit = new() { Author = "author" };
-            ModpackBuild result = await _subject.CreateRcBuild("1.1.0", githubCommit);
-
-            result.Environment.Should().Be(GameEnvironment.RC);
-            result.BuildNumber.Should().Be(1);
-            result.BuilderId.Should().Be("accountId");
-            result.EnvironmentVariables.Should().BeEquivalentTo(new Dictionary<string, object> { { "ace_updated", true }, { "acre_updated", true }, { "uksf_air_updated", true } });
-        }
+        // [Fact]
+        // public async Task When_creating_rc_build()
+        // {
+        //     _mockBuildsContext.Setup(x => x.Get(It.IsAny<Func<ModpackBuild, bool>>())).Returns(new List<ModpackBuild>());
+        //     _mockAccountContext.Setup(x => x.GetSingle(It.IsAny<Func<DomainAccount, bool>>())).Returns(new DomainAccount { Id = "accountId" });
+        //     _mockBuildStepService.Setup(x => x.GetSteps(GameEnvironment.RC)).Returns(new List<ModpackBuildStep>());
+        //
+        //     GithubCommit githubCommit = new() { Author = "author" };
+        //     ModpackBuild result = await _subject.CreateRcBuild("1.1.0", githubCommit);
+        //
+        //     result.Environment.Should().Be(GameEnvironment.RC);
+        //     result.BuildNumber.Should().Be(1);
+        //     result.BuilderId.Should().Be("accountId");
+        //     result.EnvironmentVariables.Should().BeEquivalentTo(new Dictionary<string, object> { { "ace_updated", true }, { "acre_updated", true }, { "uksf_air_updated", true } });
+        // }
     }
 }
