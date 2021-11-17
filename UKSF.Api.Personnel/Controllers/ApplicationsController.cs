29210152989b0df365e27f07026ffbb7460ec3de
Tim Beswick <contact.tim.here@gmail.com>
2021-11-17 21:57:58 +0000

    Changing routing for account applications

--- UKSF.Api.Personnel/Controllers/ApplicationsController.cs
+++ UKSF.Api.Personnel/Controllers/ApplicationsController.cs
@@ -11,39 +11,36 @@ using UKSF.Api.Shared.Extensions;
 
 namespace UKSF.Api.Personnel.Controllers
 {
-    [Route("[controller]")]
+    [Route("accounts/{id}/application"), Permissions(Permissions.CONFIRMED)]
     public class ApplicationsController : ControllerBase
     {
         private readonly IAccountContext _accountContext;
-        private readonly IAccountService _accountService;
         private readonly ILogger _logger;
         private readonly INotificationsService _notificationsService;
 
         public ApplicationsController(
             IAccountContext accountContext,
-            IAccountService accountService,
             INotificationsService notificationsService,
             ILogger logger
         )
         {
             _accountContext = accountContext;
-            _accountService = accountService;
             _notificationsService = notificationsService;
             _logger = logger;
         }
 
-        [HttpPost, Permissions(Permissions.CONFIRMED)]
-        public async Task Post([FromServices] ICreateApplicationCommand createApplicationCommand, [FromBody] Account account)
+        [HttpPost]
+        public async Task Post([FromServices] ICreateApplicationCommand createApplicationCommand, [FromRoute] string id, [FromBody] Account account)
         {
-            await createApplicationCommand.ExecuteAsync(account);
+            await createApplicationCommand.ExecuteAsync(id, account);
         }
 
-        [HttpPost("update"), Permissions(Permissions.CONFIRMED)]
-        public async Task Update([FromBody] Account account)
+        [HttpPut]
+        public async Task Update([FromRoute] string id, [FromBody] Account account)
         {
-            var domainAccount = _accountService.GetUserAccount();
+            var domainAccount = _accountContext.GetSingle(id);
             await _accountContext.Update(
-                domainAccount.Id,
+                id,
                 Builders<DomainAccount>.Update.Set(x => x.ArmaExperience, account.ArmaExperience)
                                        .Set(x => x.UnitsExperience, account.UnitsExperience)
                                        .Set(x => x.Background, account.Background)
@@ -58,12 +55,12 @@ namespace UKSF.Api.Personnel.Controllers
                     Owner = domainAccount.Application.Recruiter,
                     Icon = NotificationIcons.APPLICATION,
                     Message = $"{domainAccount.Firstname} {domainAccount.Lastname} updated their application",
-                    Link = $"/recruitment/{domainAccount.Id}"
+                    Link = $"/recruitment/{id}"
                 }
             );
 
-            var difference = domainAccount.Changes(_accountContext.GetSingle(domainAccount.Id));
-            _logger.LogAudit($"Application updated for {domainAccount.Id}: {difference}");
+            var difference = domainAccount.Changes(_accountContext.GetSingle(id));
+            _logger.LogAudit($"Application updated for {id}: {difference}");
         }
     }
 }
