7c6e97bc2606e1165d5dafc88097e5f991f0248b
Tim Beswick <contact.tim.here@gmail.com>
2022-08-21 02:01:09 +0100

    Strongly type new issue request

---
+++ UKSF.Api.Modpack/Models/NewIssueRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace UKSF.Api.Modpack.Models
+{
+    public class NewIssueRequest
+    {
+        public NewIssueType IssueType { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    public enum NewIssueType
+    {
+        WEBSITE,
+        MODPACK
+    }
+}
