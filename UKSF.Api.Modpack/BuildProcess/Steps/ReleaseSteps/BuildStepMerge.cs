803efc2c38fd1477abefa20309934bb6f13a10e3
Tim Beswick <contact.tim.here@gmail.com>
2025-07-24 22:53:22 +0100

    Fix error

--- UKSF.Api.Modpack/BuildProcess/Steps/ReleaseSteps/BuildStepMerge.cs
+++ UKSF.Api.Modpack/BuildProcess/Steps/ReleaseSteps/BuildStepMerge.cs
@@ -11,15 +11,21 @@ public class BuildStepMerge : GitBuildStep
         {
             // Necessary to get around branch protection rules for main. Runs locally on server using user stored login as credentials
             var modpackPath = Path.Join(GetBuildSourcesPath(), "modpack");
-            await GitCommand(modpackPath, "fetch");
-            await GitCommand(modpackPath, "checkout -t origin/release");
-            await GitCommand(modpackPath, "checkout release");
-            await GitCommand(modpackPath, "pull");
-            await GitCommand(modpackPath, "checkout -t origin/main");
-            await GitCommand(modpackPath, "checkout main");
-            await GitCommand(modpackPath, "pull");
-            await GitCommand(modpackPath, "merge release");
-            await GitCommand(modpackPath, "push -u origin main");
+            var gitCommand = GitService.CreateGitCommand(modpackPath);
+            await gitCommand.Execute("fetch", cancellationToken: CancellationTokenSource.Token);
+
+            await gitCommand.Execute("checkout -t origin/release", ignoreErrors: true, cancellationToken: CancellationTokenSource.Token);
+            await gitCommand.Execute("checkout release", cancellationToken: CancellationTokenSource.Token);
+
+            await gitCommand.Execute("pull", cancellationToken: CancellationTokenSource.Token);
+
+            await gitCommand.Execute("checkout -t origin/main", ignoreErrors: true, cancellationToken: CancellationTokenSource.Token);
+            await gitCommand.Execute("checkout main", cancellationToken: CancellationTokenSource.Token);
+
+            await gitCommand.Execute("pull", cancellationToken: CancellationTokenSource.Token);
+
+            await gitCommand.Execute("merge release", cancellationToken: CancellationTokenSource.Token);
+            await gitCommand.Execute("push -u origin main", cancellationToken: CancellationTokenSource.Token);
             StepLogger.Log("Release branch merge to main complete");
         }
         catch (Exception exception)
